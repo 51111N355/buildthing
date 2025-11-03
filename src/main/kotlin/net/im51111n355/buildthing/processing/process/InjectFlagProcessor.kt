16b6c97a2052bda3407207756d905e7b770bb0c3
im51111n355 <silliness.here@gmail.com>
2025-11-03 15:48:41 +0200

    @InjectFlag Fixes & Changes

--- src/main/kotlin/net/im51111n355/buildthing/processing/process/InjectFlagProcessor.kt
+++ src/main/kotlin/net/im51111n355/buildthing/processing/process/InjectFlagProcessor.kt
@@ -4,9 +4,12 @@ import net.im51111n355.buildthing.processing.BuildThingProcessor
 import net.im51111n355.buildthing.processing.BuildThingProcessor.ProcessAllAction
 import net.im51111n355.buildthing.standard.InjectFlag
 import net.im51111n355.buildthing.util.getRequiredArgument
+import net.im51111n355.buildthing.util.isStatic
 import net.im51111n355.buildthing.util.type
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Type
+import org.objectweb.asm.tree.FieldInsnNode
+import org.objectweb.asm.tree.InsnNode
 
 class InjectFlagProcessor(
     val master: BuildThingProcessor
@@ -16,6 +19,7 @@ class InjectFlagProcessor(
 
         master.processAll { classNode ->
             var modified = false
+            val injectedFields = mutableListOf<FieldInfo>()
 
             classNode.fields.forEach {
                 val isStatic = (it.access and Opcodes.ACC_STATIC) != 0
@@ -25,18 +29,47 @@ class InjectFlagProcessor(
                 it.visibleAnnotations?.removeIf { annotation ->
                     if (isInjectable && annotation.type == tIf) {
                         val value = annotation.getRequiredArgument<String>("value")
-                        it.value = value in master.config.flags
+                        it.value = if (value in master.config.flags) 1 else 0
 
+                        injectedFields.add(FieldInfo(classNode.name, it.name, it.desc))
                         modified = true
                         return@removeIf true
                     }
 
                     if (!isInjectable && annotation.type == tIf) {
                         master.project.logger.error("Field \"${it.name}\" in class \"${classNode.type.className}\" isnt static + final but has an @InjectFlag annotation!")
+                        modified = true
                         return@removeIf true
                     }
 
                     return@removeIf false
+                }
+            }
+
+            // Снос дефолт значений полей из static { ... } блока если использовали обходилку Inline'инга Java
+            // Например IS_SERVER = some() < some(); (Чтобы компилятор не совершил inline'инг) - компилятор поместит в clinit который приоритетние дефолт значения. Тут удалится put инструкция из clinit.
+            classNode.methods.forEach {
+                if (it.name != "<clinit>" || it.desc != "()V" || !it.isStatic)
+                    return@forEach
+
+                for (i in 0 until it.instructions.size()) {
+                    val insn = it.instructions[i]
+
+                    if (insn is FieldInsnNode && insn.opcode == Opcodes.PUTSTATIC) {
+                        val info = FieldInfo(insn.owner, insn.name, insn.desc)
+                        if (info !in injectedFields) continue
+
+                        // Заменить установку поля на снятие того что загрузили БЫ со стака
+                        val type = Type.getType(insn.desc)
+
+                        val newInsn = when (type) {
+                            Type.DOUBLE_TYPE, Type.LONG_TYPE -> InsnNode(Opcodes.POP2)
+                            else -> InsnNode(Opcodes.POP)
+                        }
+
+                        modified = true
+                        it.instructions.set(insn, newInsn)
+                    }
                 }
             }
 
@@ -46,4 +79,10 @@ class InjectFlagProcessor(
                 ProcessAllAction.NOT_MODIFIED
         }
     }
+
+    private data class FieldInfo(
+        val owner: String,
+        val name: String,
+        val desc: String
+    )
 }
